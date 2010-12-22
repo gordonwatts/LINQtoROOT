831ee728fa419bfa5aa1c72e49e07665fa78f522
gwatts <devnull@localhost>
2010-12-22 16:32:57 -0600

    Added some template parser files - vector<vector<int>>, etc., get correctly parsed by this guy and a small object model is built up.

---
+++ TTreeParser.Tests/t_TemplateParser.cs
@@ -0,0 +1,232 @@
+﻿using TTreeParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TTreeParser.Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for TemplateParserTest and is intended
+    ///to contain all TemplateParserTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class TemplateParserTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for ParseForTemplates
+        ///</summary>
+        [TestMethod()]
+        public void ParseForTemplatesSimpleTypeTest()
+        {
+            var r = TemplateParser.ParseForTemplates("int");
+            Assert.IsInstanceOfType(r, typeof(TemplateParser.RegularDecl), "incorrect type came back");
+            var d = r as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", d.Type, "Bad name of the decl");
+        }
+
+        [TestMethod]
+        public void ParseForTemplatesSimpleVector()
+        {
+            var r = TemplateParser.ParseForTemplates("vector<int>");
+            Assert.IsInstanceOfType(r, typeof(TemplateParser.TemplateInfo), "incorrect type came back");
+            var t = r as TemplateParser.TemplateInfo;
+            Assert.AreEqual("vector", t.TemplateName, "bad template name");
+            Assert.AreEqual(1, t.Arguments.Length, "bad # of template arguments");
+            Assert.IsInstanceOfType(t.Arguments[0], typeof(TemplateParser.RegularDecl), "bad tempmlate argument type");
+            var arg0 = t.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg0.Type);
+        }
+
+        [TestMethod]
+        public void ParseForTemplateSimpleVectorMultipleArg()
+        {
+            var r = TemplateParser.ParseForTemplates("map<int, string>");
+            Assert.IsInstanceOfType(r, typeof(TemplateParser.TemplateInfo), "incorrect type came back");
+            var t = r as TemplateParser.TemplateInfo;
+            Assert.AreEqual("map", t.TemplateName, "bad template name");
+            Assert.AreEqual(2, t.Arguments.Length, "bad # of template arguments");
+
+            Assert.IsInstanceOfType(t.Arguments[0], typeof(TemplateParser.RegularDecl), "bad tempmlate argument type");
+            var arg0 = t.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg0.Type);
+
+            Assert.IsInstanceOfType(t.Arguments[1], typeof(TemplateParser.RegularDecl), "bad tempmlate argument type");
+            var arg1 = t.Arguments[1] as TemplateParser.RegularDecl;
+            Assert.AreEqual("string", arg1.Type);
+        }
+
+        [TestMethod]
+        public void ParseForTemplateSimpleVectorNestingArg()
+        {
+            var r = TemplateParser.ParseForTemplates("vector<int, allocator<int> >");
+            Assert.IsInstanceOfType(r, typeof(TemplateParser.TemplateInfo), "incorrect type came back");
+            var t = r as TemplateParser.TemplateInfo;
+            Assert.AreEqual("vector", t.TemplateName, "bad template name");
+            Assert.AreEqual(2, t.Arguments.Length, "bad # of template arguments");
+
+            Assert.IsInstanceOfType(t.Arguments[0], typeof(TemplateParser.RegularDecl), "bad tempmlate argument type");
+            var arg0 = t.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg0.Type);
+
+            Assert.IsInstanceOfType(t.Arguments[1], typeof(TemplateParser.TemplateInfo), "bad tempmlate argument type");
+            var arg1 = t.Arguments[1] as TemplateParser.TemplateInfo;
+            Assert.AreEqual("allocator", arg1.TemplateName);
+            Assert.AreEqual(1, arg1.Arguments.Length, "incorrect # of arguments to nested template");
+            Assert.IsInstanceOfType(arg1.Arguments[0], typeof(TemplateParser.RegularDecl), "Nested argument isn't right");
+            var arg10 = arg1.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg10.Type, "incorrect nested argument type");
+        }
+
+        [TestMethod]
+        public void ParseForTemplateSimpleVectorNestingOuchArg()
+        {
+            var r = TemplateParser.ParseForTemplates("map<int, vector<int, allocator<int> >, allocator<int> >");
+
+            Assert.IsInstanceOfType(r, typeof(TemplateParser.TemplateInfo), "incorrect type came back");
+            var t = r as TemplateParser.TemplateInfo;
+            Assert.AreEqual("map", t.TemplateName, "bad template name");
+            Assert.AreEqual(3, t.Arguments.Length, "bad # of template arguments");
+
+            Assert.IsInstanceOfType(t.Arguments[0], typeof(TemplateParser.RegularDecl), "bad tempmlate argument type");
+            var arg0 = t.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg0.Type);
+
+            Assert.IsInstanceOfType(t.Arguments[1], typeof(TemplateParser.TemplateInfo), "bad tempmlate argument type");
+            var arg1 = t.Arguments[1] as TemplateParser.TemplateInfo;
+            Assert.AreEqual("vector", arg1.TemplateName);
+            Assert.AreEqual(2, arg1.Arguments.Length, "incorrect # of arguments to nested template");
+            Assert.IsInstanceOfType(arg1.Arguments[0], typeof(TemplateParser.RegularDecl), "vector 1st arg not right");
+            Assert.IsInstanceOfType(arg1.Arguments[1], typeof(TemplateParser.TemplateInfo), "vector 2nd arg not right");
+            var arg10 = arg1.Arguments[0] as TemplateParser.RegularDecl;
+            var arg11 = arg1.Arguments[1] as TemplateParser.TemplateInfo;
+            Assert.AreEqual("int", arg10.Type, "type of vector's 1st arg not right");
+            Assert.AreEqual("allocator", arg11.TemplateName, "expected name of seocond tempmlate arg to vector not right");
+            Assert.AreEqual(1, arg11.Arguments.Length, "incorrect # of arguments to allocator of vector");
+            Assert.IsInstanceOfType(arg11.Arguments[0], typeof(TemplateParser.RegularDecl), "bad allocator argument type");
+            var arg110 = arg11.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg110.Type, "bad type for allcoator argument of vector");
+
+            Assert.IsInstanceOfType(t.Arguments[2], typeof(TemplateParser.TemplateInfo), "bad tempmlate argument type");
+            var arg2 = t.Arguments[2] as TemplateParser.TemplateInfo;
+            Assert.AreEqual("allocator", arg2.TemplateName);
+            Assert.AreEqual(1, arg2.Arguments.Length, "incorrect # of arguments to allocator of map");
+            Assert.IsInstanceOfType(arg2.Arguments[0], typeof(TemplateParser.RegularDecl), "bad allocator argument type for map");
+            var arg20 = arg11.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg20.Type, "bad type for allcoator argument of map");
+        }
+
+        [TestMethod]
+        public void ParseForTemplateSimpleVectorNestingOuchNoSpacesArg()
+        {
+            var r = TemplateParser.ParseForTemplates("map<int,vector<int,allocator<int> >,allocator<int> >");
+
+            Assert.IsInstanceOfType(r, typeof(TemplateParser.TemplateInfo), "incorrect type came back");
+            var t = r as TemplateParser.TemplateInfo;
+            Assert.AreEqual("map", t.TemplateName, "bad template name");
+            Assert.AreEqual(3, t.Arguments.Length, "bad # of template arguments");
+
+            Assert.IsInstanceOfType(t.Arguments[0], typeof(TemplateParser.RegularDecl), "bad tempmlate argument type");
+            var arg0 = t.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg0.Type);
+
+            Assert.IsInstanceOfType(t.Arguments[1], typeof(TemplateParser.TemplateInfo), "bad tempmlate argument type");
+            var arg1 = t.Arguments[1] as TemplateParser.TemplateInfo;
+            Assert.AreEqual("vector", arg1.TemplateName);
+            Assert.AreEqual(2, arg1.Arguments.Length, "incorrect # of arguments to nested template");
+            Assert.IsInstanceOfType(arg1.Arguments[0], typeof(TemplateParser.RegularDecl), "vector 1st arg not right");
+            Assert.IsInstanceOfType(arg1.Arguments[1], typeof(TemplateParser.TemplateInfo), "vector 2nd arg not right");
+            var arg10 = arg1.Arguments[0] as TemplateParser.RegularDecl;
+            var arg11 = arg1.Arguments[1] as TemplateParser.TemplateInfo;
+            Assert.AreEqual("int", arg10.Type, "type of vector's 1st arg not right");
+            Assert.AreEqual("allocator", arg11.TemplateName, "expected name of seocond tempmlate arg to vector not right");
+            Assert.AreEqual(1, arg11.Arguments.Length, "incorrect # of arguments to allocator of vector");
+            Assert.IsInstanceOfType(arg11.Arguments[0], typeof(TemplateParser.RegularDecl), "bad allocator argument type");
+            var arg110 = arg11.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg110.Type, "bad type for allcoator argument of vector");
+
+            Assert.IsInstanceOfType(t.Arguments[2], typeof(TemplateParser.TemplateInfo), "bad tempmlate argument type");
+            var arg2 = t.Arguments[2] as TemplateParser.TemplateInfo;
+            Assert.AreEqual("allocator", arg2.TemplateName);
+            Assert.AreEqual(1, arg2.Arguments.Length, "incorrect # of arguments to allocator of map");
+            Assert.IsInstanceOfType(arg2.Arguments[0], typeof(TemplateParser.RegularDecl), "bad allocator argument type for map");
+            var arg20 = arg11.Arguments[0] as TemplateParser.RegularDecl;
+            Assert.AreEqual("int", arg20.Type, "bad type for allcoator argument of map");
+        }
+
+        [TestMethod]
+        public void ParseForTemplateSimpleVectorTrippleArg()
+        {
+            var r = TemplateParser.ParseForTemplates("vector<vector<vector<int> > >");
+            Assert.IsInstanceOfType(r, typeof(TemplateParser.TemplateInfo), "incorrect type came back");
+            var t = r as TemplateParser.TemplateInfo;
+            Assert.AreEqual("vector", t.TemplateName, "bad template name");
+            Assert.AreEqual(1, t.Arguments.Length, "bad # of template arguments");
+            Assert.IsInstanceOfType(t.Arguments[0], typeof(TemplateParser.TemplateInfo), "vector's argument bad type");
+            var arg0 = t.Arguments[0] as TemplateParser.TemplateInfo;
+
+            Assert.AreEqual("vector", arg0.TemplateName, "bad template argument name");
+            Assert.AreEqual(1, arg0.Arguments.Length, "bad # of arguments to vector<vector template");
+            Assert.IsInstanceOfType(arg0.Arguments[0], typeof(TemplateParser.TemplateInfo), "bad return type for vector<vector argument");
+            var arg00 = arg0.Arguments[0] as TemplateParser.TemplateInfo;
+
+            Assert.AreEqual("vector", arg00.TemplateName, "bad template argument name");
+            Assert.AreEqual(1, arg00.Arguments.Length, "bad # of arguments to vector<vector template");
+            Assert.IsInstanceOfType(arg00.Arguments[0], typeof(TemplateParser.RegularDecl), "bad return type for vector<vector argument");
+            var arg000 = arg00.Arguments[0] as TemplateParser.RegularDecl;
+
+            Assert.AreEqual("int", arg000.Type, "bad type for innermost template");
+        }
+    }
+}
---
+++ TTreeParser/TemplateParser.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TTreeParser
+{
+    /// <summary>
+    /// Aid with parsing C++ template classes
+    /// </summary>
+    public static class TemplateParser
+    {
+        public interface IDecl
+        {
+        }
+
+        public class TemplateInfo : IDecl
+        {
+            public string TemplateName { get; private set; }
+
+            public IDecl[] Arguments { get; private set; }
+
+            public TemplateInfo(string templateName, IDecl[] args)
+            {
+                TemplateName = templateName;
+                Arguments = args;
+            }
+        }
+
+        public class RegularDecl : IDecl
+        {
+            /// <summary>
+            /// Get the type string for this declaration.
+            /// </summary>
+            public string Type { get; private set; }
+
+            public RegularDecl(string typeDef)
+            {
+                Type = typeDef;
+            }
+        }
+
+        private static Regex templateParameterFinder = new Regex("(?<tclass>\\w+)\\<(?<types>.*)\\>$");
+
+        /// <summary>
+        /// Given a declaration return a structure which pulls it apart, including all template
+        /// arguments. It deals correctly with nested templates as well.
+        /// </summary>
+        /// <param name="typeDef"></param>
+        public static IDecl ParseForTemplates(this string typeDef)
+        {
+            if (typeDef == null)
+                throw new ArgumentNullException("typeDef");
+
+            ///
+            /// Is the current typeDef a template?
+            /// 
+
+            var m = templateParameterFinder.Match(typeDef);
+            if (!m.Success)
+            {
+                return new RegularDecl(typeDef);
+            }
+
+            ///
+            /// Now we have to carefully split up arguments. This is painful! We split by ",", but we
+            /// have to be careful of sub template definitions that might contain commas.
+            /// 
+
+            var argStrings = SplitTemplateArguments(m.Groups["types"].Value);
+            var args = (from a in argStrings
+                        select ParseForTemplates(a)).ToArray();
+
+            ///
+            /// Ok, now create a new template definition from that!
+            /// 
+
+            var t = new TemplateInfo(m.Groups["tclass"].Value, args);
+            
+            return t;
+        }
+
+        /// <summary>
+        /// Split template arguments up (by the ".", but ignore sub-template definitions.
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        private static string[] SplitTemplateArguments(string argumentString)
+        {
+            string toParse = argumentString.Trim();
+            List<string> result = new List<string>();
+
+            while (toParse.Length > 0)
+            {
+                int locationOfNextGoodComma = FindNextNonTemplateComma(toParse);
+                if (locationOfNextGoodComma < 0)
+                {
+                    result.Add(toParse);
+                    toParse = "";
+                } else {
+                    result.Add(toParse.Substring(0, locationOfNextGoodComma));
+                    toParse = toParse.Substring(locationOfNextGoodComma + 1).Trim();
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Go forward looking for the next comma that isn't in a <>.
+        /// </summary>
+        /// <param name="toParse"></param>
+        /// <returns></returns>
+        private static int FindNextNonTemplateComma(string toParse)
+        {
+            int index = 0;
+            while (index < toParse.Length)
+            {
+                if (toParse[index] == ',')
+                {
+                    return index;
+                }
+                else if (toParse[index] == '<')
+                {
+                    index += FindMatchingClosingBraket(toParse.Substring(index+1));
+                }
+                index += 1;
+            }
+
+            ///
+            /// When we drop through here we didn't find a comma!
+            /// 
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Look for the matching closing braket...
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        private static int FindMatchingClosingBraket(string p)
+        {
+            int index = 0;
+            while (index < p.Length)
+            {
+                if (p[index] == '>')
+                    return index;
+
+                if (p[index] == '<')
+                {
+                    index += FindMatchingClosingBraket(p.Substring(index+1));
+                }
+                else
+                {
+                    index += 1;
+                }
+            }
+
+            return p.Length;
+        }
+    }
+}
