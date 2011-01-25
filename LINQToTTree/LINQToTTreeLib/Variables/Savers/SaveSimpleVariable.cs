799037492f9b37c531cc0cbce27cc599539a787f
gwatts <devnull@localhost>
2011-01-24 20:15:04 -0800

    No longer need to reference NTH1I as the modern root sublayer (2.1 or later) now correctly loads things that haven't been loaded yet.

--- LINQToTTree/LINQToTTreeLib/Variables/Savers/SaveSimpleVariable.cs
+++ LINQToTTree/LINQToTTreeLib/Variables/Savers/SaveSimpleVariable.cs
@@ -31,19 +31,6 @@ namespace LINQToTTreeLib.Variables.Savers
         /// <returns></returns>
         public IEnumerable<string> SaveToFile(IVariable iVariable)
         {
-            ///
-            /// .NET is very cleaver about how it loads up the various libraries. It waits until the absolutely last minute. This
-            /// means that right now, there is a good chance that libHistWrappers is not loaded. The unfortunate by-product of thsi is that
-            /// when we go to load in the NTH1I object it won't be known, and so we won't get a good wrapper. Until there is auto-loading
-            /// of wrappers by the ROOT.NET sub-system, we have to force the issue right now with a reference.
-            /// 
-
-            var h = new ROOTNET.NTH1I(); // Force loading of the libHistWrappers early enough!
-
-            ///
-            /// Emit source code to save this guy.
-            /// 
-
             yield return "TH1I *" + iVariable.RawValue + "_hist = new TH1I(\"" + iVariable.RawValue + "\", \"var transport\", 1, 0.0, 1.0);";
             yield return iVariable.RawValue + "_hist->SetBinContent(1, " + iVariable.RawValue + ");";
             yield return "Book(" + iVariable.RawValue + "_hist);";
