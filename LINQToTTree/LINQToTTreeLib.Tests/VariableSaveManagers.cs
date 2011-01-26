d738dc1ca9cb2e22991e831ff5ec651fe70a8531
gwatts <devnull@localhost>
2011-01-25 23:47:52 -0800

    Some simple Var Saver getters to use in tests. BTW, one of the classes in here makes Pex totall fail.

---
+++ LINQToTTree/LINQToTTreeLib.Tests/VariableSaveManagers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using LinqToTTreeInterfacesLib;
+
+namespace LINQToTTreeLib.Tests
+{
+    /// <summary>
+    /// Basic VM that returns a guy that always does something simple.
+    /// </summary>
+    [Export(typeof(IVariableSaverManager))]
+    class VSBasic : IVariableSaverManager
+    {
+        public IVariableSaver Get(IVariable var)
+        {
+            if (var == null)
+                throw new NotImplementedException("This should never happen");
+            return new SimpleVar();
+        }
+
+        public class SimpleVar : IVariableSaver
+        {
+            public bool CanHandle(IVariable iVariable)
+            {
+                return true;
+            }
+
+            public System.Collections.Generic.IEnumerable<string> SaveToFile(IVariable iVariable)
+            {
+                yield return "save";
+            }
+
+            public System.Collections.Generic.IEnumerable<string> IncludeFiles(IVariable iVariable)
+            {
+                yield return "file.h";
+            }
+
+            public T LoadResult<T>(IVariable iVariable, ROOTNET.Interface.NTObject obj)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
