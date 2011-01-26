99ae910ee9e7647dc9a70abc89a24cbbec82ba01
gwatts <devnull@localhost>
2011-01-25 23:47:21 -0800

    Seperate out the variable manager so we can help with testing.

---
+++ LINQToTTree/LinqToTTreeInterfacesLib/IVariableSaverManager.cs
@@ -0,0 +1,16 @@
+﻿
+namespace LinqToTTreeInterfacesLib
+{
+    /// <summary>
+    /// Simple interface to get the variable saver guys
+    /// </summary>
+    public interface IVariableSaverManager
+    {
+        /// <summary>
+        /// Returns a variable saver for the particular variable.
+        /// </summary>
+        /// <param name="var"></param>
+        /// <returns></returns>
+        IVariableSaver Get(IVariable var);
+    }
+}
