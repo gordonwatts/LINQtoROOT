63e8e36c8b58383e095edfcc0b129d5c8ddfb651
gwatts <devnull@localhost>
2011-01-26 23:17:40 -0800

    Added variable saver to deal with objects (like ROOT objects) so that we can get the results back to the client!

---
+++ LINQToTTree/LINQToTTreeLib.Tests/Variables/Savers/SaveVarObjectTest.cs
@@ -0,0 +1,81 @@
+// <copyright file="SaveVarObjectTest.cs" company="Microsoft">Copyright © Microsoft 2010</copyright>
+using System;
+using System.Collections.Generic;
+using LinqToTTreeInterfacesLib;
+using Microsoft.Pex.Framework;
+using Microsoft.Pex.Framework.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ROOTNET.Interface;
+
+namespace LINQToTTreeLib.Variables.Savers
+{
+    /// <summary>This class contains parameterized unit tests for SaveVarObject</summary>
+    [PexClass(typeof(SaveVarObject))]
+    [PexAllowedExceptionFromTypeUnderTest(typeof(InvalidOperationException))]
+    [PexAllowedExceptionFromTypeUnderTest(typeof(ArgumentException), AcceptExceptionSubtypes = true)]
+    [TestClass]
+    public partial class SaveVarObjectTest
+    {
+        /// <summary>Test stub for CanHandle(IVariable)</summary>
+        [PexMethod]
+        internal bool CanHandle([PexAssumeUnderTest]SaveVarObject target, IVariable iVariable)
+        {
+            bool result = target.CanHandle(iVariable);
+            return result;
+            // TODO: add assertions to method SaveVarObjectTest.CanHandle(SaveVarObject, IVariable)
+        }
+
+        [TestMethod]
+        public void TestForNonROOTObject()
+        {
+            var v = new VarObject(typeof(int[]));
+            Assert.IsFalse(CanHandle(new SaveVarObject(), v), "int array should be false");
+        }
+
+        [TestMethod]
+        public void TestForROOTTObject()
+        {
+            var v = new VarObject(typeof(ROOTNET.NTObject));
+            Assert.IsFalse(CanHandle(new SaveVarObject(), v), "TObject array should be false");
+        }
+
+        [TestMethod]
+        public void TestForROOTTH1F()
+        {
+            var v = new VarObject(typeof(ROOTNET.NTH1F));
+            Assert.IsTrue(CanHandle(new SaveVarObject(), v), "TH1F");
+        }
+
+        /// <summary>Test stub for IncludeFiles(IVariable)</summary>
+        [PexMethod]
+        internal IEnumerable<string> IncludeFiles([PexAssumeUnderTest]SaveVarObject target, IVariable iVariable)
+        {
+            IEnumerable<string> result = target.IncludeFiles(iVariable);
+            return result;
+            // TODO: add assertions to method SaveVarObjectTest.IncludeFiles(SaveVarObject, IVariable)
+        }
+
+        /// <summary>Test stub for LoadResult(IVariable, NTObject)</summary>
+        [PexGenericArguments(typeof(int))]
+        [PexMethod]
+        internal T LoadResult<T>(
+            [PexAssumeUnderTest]SaveVarObject target,
+            IVariable iVariable,
+            NTObject obj
+        )
+        {
+            T result = target.LoadResult<T>(iVariable, obj);
+            return result;
+            // TODO: add assertions to method SaveVarObjectTest.LoadResult(SaveVarObject, IVariable, NTObject)
+        }
+
+        /// <summary>Test stub for SaveToFile(IVariable)</summary>
+        [PexMethod]
+        internal IEnumerable<string> SaveToFile([PexAssumeUnderTest]SaveVarObject target, IVariable iVariable)
+        {
+            IEnumerable<string> result = target.SaveToFile(iVariable);
+            return result;
+            // TODO: add assertions to method SaveVarObjectTest.SaveToFile(SaveVarObject, IVariable)
+        }
+    }
+}
--- LINQToTTree/LINQToTTreeLib/Variables/Savers/SaveVarObject.cs
+++ LINQToTTree/LINQToTTreeLib/Variables/Savers/SaveVarObject.cs
@@ -1,53 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
 using LinqToTTreeInterfacesLib;
 
 namespace LINQToTTreeLib.Variables.Savers
 {
     /// <summary>
-    /// Save simple guys - like an integer.
+    /// Manage taking some object accross the wire back to the client once the processing
+    /// of the tree is complete. At the moment we know only how to deal with ROOT objects! :-)
     /// </summary>
     [Export(typeof(IVariableSaver))]
-    public class SaveSimpleVariable : IVariableSaver
+    public class SaveVarObject : IVariableSaver
     {
         /// <summary>
-        /// We can deal with things like integers.
+        /// Return OK for any VarObject that is a named root object.
         /// </summary>
         /// <param name="iVariable"></param>
         /// <returns></returns>
         public bool CanHandle(IVariable iVariable)
         {
-            if (iVariable.GetType() == typeof(VarInteger))
-                return true;
-
+            if ((iVariable as VarObject) != null)
+            {
+                return iVariable.Type.GetInterface("NTNamed") != null;
+            }
             return false;
         }
 
         /// <summary>
-        /// Generate the code that will save this guy to a file.
+        /// The lines of code that can be used to save this guy to a file. This is actually quite easy
+        /// - we just book it!
         /// </summary>
         /// <param name="iVariable"></param>
         /// <returns></returns>
         public IEnumerable<string> SaveToFile(IVariable iVariable)
         {
-            yield return "TH1I *" + iVariable.RawValue + "_hist = new TH1I(\"" + iVariable.RawValue + "\", \"var transport\", 1, 0.0, 1.0);";
-            yield return iVariable.RawValue + "_hist->SetBinContent(1, " + iVariable.RawValue + ");";
-            yield return "Book(" + iVariable.RawValue + "_hist);";
+            StringBuilder bld = new StringBuilder();
+            bld.AppendFormat("{0}->SetName(\"{0}\");", iVariable.RawValue);
+            yield return bld.ToString();
+            yield return "Book(" + iVariable.RawValue + ");";
         }
 
         /// <summary>
-        /// what include files do we need?
+        /// In order to setup the above code we do not need to implement any special code... or pull anything extra in.
         /// </summary>
         /// <param name="iVariable"></param>
         /// <returns></returns>
         public IEnumerable<string> IncludeFiles(IVariable iVariable)
         {
-            yield return "TH1I.h";
+            return Enumerable.Empty<string>();
         }
 
         /// <summary>
-        /// We need to load the variable saver back in.
+        /// Ok, back on the client with the result. We need to try and load the thing in.
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="iVariable"></param>
@@ -55,12 +60,7 @@ namespace LINQToTTreeLib.Variables.Savers
         /// <returns></returns>
         public T LoadResult<T>(IVariable iVariable, ROOTNET.Interface.NTObject obj)
         {
-            var intHist = obj as ROOTNET.NTH1I;
-            if (intHist == null)
-                throw new InvalidOperationException("Object of type '" + obj.ClassName() + "' is not an integer histogram, which is what we were expecting for this result!");
-
-            object result = (int)intHist.GetBinContent(1);
-            return (T)result;
+            return (T)obj;
         }
     }
 }
