77d9d7fa4ec4468b518b0c2943a088d7d1858111
gwatts <devnull@localhost>
2011-01-28 16:15:34 -0800

    Mising file adeded

---
+++ LINQToTTree/TTreeDataModel/NtupleTreeInfo.cs
@@ -0,0 +1,20 @@
+﻿
+namespace TTreeDataModel
+{
+    /// <summary>
+    /// Info that is needed to write out info about a ntuple. Basically, the list
+    /// of classes and any C++ files that need to be loaded or compiled.
+    /// </summary>
+    public class NtupleTreeInfo
+    {
+        /// <summary>
+        /// The list of classes that will need to be used
+        /// </summary>
+        public ROOTClassShell[] Classes;
+
+        /// <summary>
+        /// List of the files required to run this guy
+        /// </summary>
+        public string[] ClassImplimintationFiles;
+    }
+}
