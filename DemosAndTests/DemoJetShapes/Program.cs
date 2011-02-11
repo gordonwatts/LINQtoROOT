2fa230ebfa9dcaada0fed82b3832275b603b310c
gwatts <devnull@localhost>
2011-02-10 20:23:07 -0800

    New test code - mostly doesn't work, so it is commented out.

--- DemosAndTests/DemoJetShapes/Program.cs
+++ DemosAndTests/DemoJetShapes/Program.cs
@@ -2,11 +2,11 @@
 /// Simple demo program that shows one how to do some very basic things
 /// with LINQ and a local ntuple.
 /// 
+
 using System;
 using System.IO;
 using System.Linq;
 using ROOTTTreeDataModel;
-
 namespace DemoJetShapes
 {
     class Program
@@ -17,6 +17,7 @@ namespace DemoJetShapes
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+
             FileInfo rootFile = new FileInfo(@"..\..\..\output.root");
             if (!rootFile.Exists)
             {
@@ -24,7 +25,6 @@ namespace DemoJetShapes
                 return;
             }
 
-            //var rf = new Queryablebtag(rootFile);
             var rf1 = Queryablebtag.Create(rootFile);
 
             ///
@@ -33,6 +33,34 @@ namespace DemoJetShapes
 
             int count = rf1.Count();
             Console.WriteLine("The number of events in the ntuple, counted the hard way is {0}.", count);
+
+            ROOTNET.Interface.NBTagJet b;
+#if false
+
+            ///
+            /// Next, count the number of jets in the file
+            /// 
+
+            var alljets = from e in rf1
+                          from j in e.jets
+                          select j;
+
+            Console.WriteLine("The number of jets in the ntuple is {0}.", alljets.Count());
+
+            ///
+            /// Apply some very simple cuts
+            /// 
+
+            var ptCutJets = from j in alljets
+                            where j.Pt() > 30.0
+                            select j;
+            Console.WriteLine("The number of jets with a pT greater than 30 GeV is {0}", ptCutJets.Count());
+
+            var etaCutJets = from j in alljets
+                             where Math.Abs(j.Eta()) < 1.0
+                             select j;
+            Console.WriteLine("The number of jets with an eta less than 1.0 is {0}", etaCutJets.Count());
+#endif
         }
     }
 }
