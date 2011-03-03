a29e97645a1bbe4ae9dbccba219fd5967f813c68
gwatts <devnull@localhost>
2011-03-03 07:15:21 +0100

    Demo project for a large ntuple with connected arrays.

---
+++ DemosAndTests/DemoNtupleArrays/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DemoNtupleArrays
+{
+    class Program
+    {
+        /// <summary>
+        /// This demo shows how to work with an ntuple that contains a large number of arrays that
+        /// are actually linked together (that is. pt, eta, phi, one entry for each jet). You will need
+        /// to look at the GenerateArrayNtupleXMLSpec for the generation of the items.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
