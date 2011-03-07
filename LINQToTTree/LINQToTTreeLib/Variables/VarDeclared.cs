b3223fc5f56da37f6f6fb1928d018be0a24dcce2
gwatts <devnull@localhost>
2011-03-07 02:25:02 -0800

    Missing file added.

---
+++ LINQToTTree/LINQToTTreeLib/Variables/VarDeclared.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using LinqToTTreeInterfacesLib;
+
+namespace LINQToTTreeLib.Variables
+{
+    /// <summary>
+    /// A simple variable class for something that is already declared and being used
+    /// in the code - in short, not something that will have to be used later on.
+    /// </summary>
+    public class VarDeclared : IVariable
+    {
+        private IValue _value;
+        private string _name;
+
+        public VarDeclared(IValue iValue, string indexName)
+        {
+            // TODO: Complete member initialization
+            _value = iValue;
+            _name = indexName;
+        }
+        public string VariableName
+        {
+            get { return _name; }
+        }
+
+        public IValue InitialValue
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public bool Declare
+        {
+            get
+            {
+                return false;
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string RawValue
+        {
+            get { return _value.RawValue; }
+        }
+
+        public Type Type
+        {
+            get { return _value.Type; }
+        }
+    }
+}
