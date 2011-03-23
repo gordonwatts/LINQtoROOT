b9459d12e4ac23ad6ae91bdc9ef7f0d2aad1521e
gwatts <devnull@localhost>
2011-03-22 22:18:12 -0700

    Added new code to help with dealing with arrays in this new world - they keep track of expressions rather than IValues/etc. That way we can hold onto the expressions and only do the (complete) reconstruction at the very last minute. This is part of enabling translation utilities.

---
+++ LINQToTTree/LINQToTTreeLib.Tests/t_ArrayExpressionParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+using LINQToTTreeLib.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LINQToTTreeLib.Tests
+{
+    /// <summary>
+    ///This is a test class for TestArrayExpressionParser and is intended
+    ///to contain all TestArrayExpressionParser Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class TestArrayExpressionParser
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestForNonArray()
+        {
+            ArrayExpressionParser.ParseArrayExpression(Expression.Variable(typeof(int), "d"));
+        }
+
+        [TestMethod]
+        public void TestRunForNormalArray()
+        {
+            ArrayExpressionParser.ParseArrayExpression(Expression.Variable(typeof(int[]), "d"));
+        }
+    }
+}
---
+++ LINQToTTree/LINQToTTreeLib/Expressions/ArrayInfoVector.cs
@@ -0,0 +1,116 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using LinqToTTreeInterfacesLib;
+using LINQToTTreeLib.Statements;
+using LINQToTTreeLib.Variables;
+
+namespace LINQToTTreeLib.Expressions
+{
+    /// <summary>
+    /// Holds the smarts to deal with an array info that is represented as vector in the C++ code.
+    /// </summary>
+    internal class ArrayInfoVector : IArrayInfo
+    {
+        /// <summary>
+        /// Holds onto the array expression that we are going to be working against.
+        /// </summary>
+        private Expression _arrayExpression;
+
+        /// <summary>
+        /// Create a new array info, using expr as the base for our work.
+        /// </summary>
+        /// <param name="expr"></param>
+        public ArrayInfoVector(Expression expr)
+        {
+            this._arrayExpression = expr;
+        }
+
+        /// <summary>
+        /// Generate the code that we will use to access this array. Loop symantics in this framework are basically "foreach" rather than "for" - so
+        /// we return an object that can be used to reference each array element.
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="context"></param>
+        /// <param name="indexName"></param>
+        /// <param name="popVariableContext"></param>
+        /// <returns></returns>
+        public System.Linq.Expressions.Expression AddLoop(IGeneratedCode env, ICodeContext context)
+        {
+            ///
+            /// First, we will need to know the length of this array
+            /// 
+
+            var lenExpression = Expression.ArrayLength(_arrayExpression);
+            var lenTranslation = ExpressionVisitor.GetExpression(lenExpression, env, context);
+
+            ///
+            /// Next, generate the expression that forms the basis of the index lookup. We don't
+            /// translate this - that only gets to happen when one is actually looking at a final result.
+            /// 
+
+            var loopVariable = Expression.Variable(typeof(int), typeof(int).CreateUniqueVariableName());
+            var indexExpression = Expression.MakeBinary(ExpressionType.ArrayIndex, _arrayExpression, loopVariable);
+
+            ///
+            /// Ok, now we are ready to actually generate some code here! First, cache the size
+            /// 
+
+            var arraySizeVar = new VarSimple(typeof(int)) { Declare = true };
+            env.Add(arraySizeVar);
+            env.Add(new StatementAssign(arraySizeVar, lenTranslation));
+
+            ///
+            /// Now the for loop statement!
+            /// 
+
+            env.Add(new StatementVectorLoop(loopVariable, arraySizeVar));
+
+            ///
+            /// Return the index expression - the thing that can be used to replace all expressions and
+            /// reference the item we are looping over.
+            /// 
+
+            return indexExpression;
+        }
+
+        /// <summary>
+        /// A local class to implement the looping statements to work over this array.
+        /// </summary>
+        private class StatementVectorLoop : StatementInlineBlock
+        {
+            /// <summary>
+            /// The loop that we will run over
+            /// </summary>
+            private string _forLoop;
+
+            /// <summary>
+            /// Create a for loop statement.
+            /// </summary>
+            /// <param name="loopVariable"></param>
+            /// <param name="arraySizeVar"></param>
+            public StatementVectorLoop(ParameterExpression loopVariable, VarSimple arraySizeVar)
+            {
+                var bld = new StringBuilder();
+                bld.AppendFormat("for (int {0}=0; {0} < {1}; {0}++)", loopVariable.Name, arraySizeVar.RawValue);
+                _forLoop = bld.ToString();
+            }
+
+            /// <summary>
+            /// Generate the code to do the looping. No need to generate anything if there is nothing to do! :-)
+            /// </summary>
+            /// <returns></returns>
+            public override System.Collections.Generic.IEnumerable<string> CodeItUp()
+            {
+                if (Statements.Any())
+                {
+                    yield return _forLoop;
+                    foreach (var l in base.CodeItUp())
+                    {
+                        yield return l;
+                    }
+                }
+            }
+        }
+    }
+}
