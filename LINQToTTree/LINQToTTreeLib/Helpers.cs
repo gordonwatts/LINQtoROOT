94510c3ee9217bf83f837d4afda70c277e56ae54
gwatts <devnull@localhost>
2011-03-26 01:38:10 -0700

    Added some things to help with Plots, etc. - in short, attempt to hide some of what is goign on with Expression<> etc. from the user.

--- LINQToTTree/LINQToTTreeLib/Helpers.cs
+++ LINQToTTree/LINQToTTreeLib/Helpers.cs
@@ -66,5 +66,76 @@ namespace LINQToTTreeLib
             return source.Aggregate(seed, (s, n) => ApplyReturnFirst(s, n, apply));
 #endif
         }
+
+        /// <summary>
+        /// Create a TH1F plot from a stream of objects (with a lambda function to give flexability in conversion).
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="plotID"></param>
+        /// <param name="plotTitle"></param>
+        /// <param name="nbins"></param>
+        /// <param name="lowBin"></param>
+        /// <param name="highBin"></param>
+        /// <param name="getter"></param>
+        /// <returns></returns>
+        public static ROOTNET.Interface.NTH1F Plot<TSource>
+            (
+            this IQueryable<TSource> source,
+            string plotID, string plotTitle,
+            int nbins, double lowBin, double highBin,
+            Expression<Func<TSource, double>> getter)
+        {
+            var hParameter = Expression.Parameter(typeof(ROOTNET.NTH1F), "h");
+            var vParameter = Expression.Parameter(typeof(TSource), "v");
+
+            /// h.Fill(getter(v)) is what we want to code up
+
+            var callGetter = Expression.Invoke(getter, vParameter);
+
+            var fillMethod = typeof(ROOTNET.NTH1F).GetMethod("Fill", new Type[] { typeof(double) });
+            var callFill = Expression.Call(hParameter, fillMethod, callGetter);
+
+            var lambda = Expression.Lambda<Action<ROOTNET.NTH1F, TSource>>(callFill, hParameter, vParameter);
+            return source.ApplyToObject(new ROOTNET.NTH1F(plotID, plotTitle, nbins, lowBin, highBin), lambda);
+        }
+
+        /// <summary>
+        /// Generate a TH1F from a stream of T's that can be converted to a double.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="plotID"></param>
+        /// <param name="plotTitle"></param>
+        /// <param name="nbins"></param>
+        /// <param name="lowBin"></param>
+        /// <param name="highBin"></param>
+        /// <returns></returns>
+        public static ROOTNET.Interface.NTH1F Plot
+            (
+            this IQueryable<double> source,
+            string plotID, string plotTitle,
+            int nbins, double lowBin, double highBin)
+        {
+            return source.Plot(plotID, plotTitle, nbins, lowBin, highBin, v => v);
+        }
+        public static ROOTNET.Interface.NTH1F Plot<T>
+            (
+            this IQueryable<float> source,
+            string plotID, string plotTitle,
+            int nbins, double lowBin, double highBin)
+        {
+            return source.Plot(plotID, plotTitle, nbins, lowBin, highBin, v => v);
+        }
+
+        /// <summary>
+        /// Add a histogram to a ROOT directory.
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <param name="h"></param>
+        public static void Add(this ROOTNET.Interface.NTDirectory dir, ROOTNET.Interface.NTH1 h)
+        {
+            h.SetDirectory(dir);
+        }
     }
 }
