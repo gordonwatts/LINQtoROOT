62c3dae14e2a4c5cc0dbf41a752b86e2c235ffaf
gwatts <devnull@localhost>
2011-03-26 01:42:39 -0700

    A demo to show off a group rename

--- DemosAndTests/DemoNtupleArrays/Program.cs
+++ DemosAndTests/DemoNtupleArrays/Program.cs
@@ -1,4 +1,11 @@
 ﻿
+using System;
+using System.IO;
+using System.Linq;
+
+/// Includes things in the Helper object, which has some nice extension methods
+using LINQToTTreeLib;
+
 namespace DemoNtupleArrays
 {
     class Program
@@ -11,6 +18,25 @@ namespace DemoNtupleArrays
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            var f = new FileInfo(@"..\..\..\btag-slim.root");
+            if (!f.Exists)
+            {
+                Console.WriteLine("could not find btag input files: " + f.FullName);
+                return;
+            }
+
+            var evts = ROOTTTreeDataModel.Queryablevtuple.Create(f);
+
+            var trackPt = from e in evts
+                          from t in e.tracks
+                          select t.pt / 1000.0;
+
+            var output = new ROOTNET.NTFile("output.root", "RECREATE");
+            output.Add(trackPt.Plot("trackpt", "Track p_{T}", 100, 0.0, 200.0));
+
+            output.Write();
+            output.Clone();
+
         }
     }
 }
