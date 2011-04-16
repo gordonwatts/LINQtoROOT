9b8a64cd7bb4a2133713dc33878fc05f8902824b
gwatts <devnull@localhost>
2011-04-16 18:46:52 +0200

    Added a MSBuild task to run the ntup -> class during the build.

---
+++ LINQToTTree/MSBuildTasks/BuildTTreeDataModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuildTasks
+{
+    /// <summary>
+    /// This task will take input ntuple spec files and produce output spec files. It looks only
+    /// for the ntupom file. The other files are just "meta-data" for this task and are internally
+    /// </summary>
+    public class BuildTTreeDataModel : Task
+    {
+        /// <summary>
+        /// List of input files - .ntupom files - that we will process to make an
+        /// output file.
+        /// </summary>
+        [Required]
+        public ITaskItem[] InputFiles { get; set; }
+
+        /// <summary>
+        /// Output .cs files that contain everything required to make and use the object model
+        /// </summary>
+        [Output]
+        public ITaskItem[] OutputFiles { get; set; }
+
+        /// <summary>
+        /// The namespace we will use to put all the items in
+        /// </summary>
+        public string Namespace { get; set; }
+
+        /// <summary>
+        /// Run the task!
+        /// </summary>
+        /// <returns></returns>
+        public override bool Execute()
+        {
+            ///
+            /// Go through each of the input items and match them to output items, and parse them.
+            /// 
+
+            var pairedFiles = InputFiles.Zip(OutputFiles, (i1, i2) => Tuple.Create(i1, i2));
+
+            foreach (var p in pairedFiles)
+            {
+                RenderDataModel(p.Item1, p.Item2);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Render and parse one of the items.
+        /// </summary>
+        /// <param name="iTaskItem"></param>
+        /// <param name="iTaskItem_2"></param>
+        private void RenderDataModel(ITaskItem inputDM, ITaskItem outputFile)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(Namespace))
+                    Namespace = "ROOTLINQ";
+
+                var inputXMLFile = new FileInfo(inputDM.GetMetadata("FullPath"));
+                var outputCSFile = new FileInfo(outputFile.GetMetadata("FullPath"));
+                var generator = new TTreeClassGenerator.ClassGenerator();
+                generator.GenerateClasss(inputXMLFile, outputCSFile, Namespace);
+            }
+            catch (Exception e)
+            {
+                Log.LogError("ParseError", "ParseError", "ParseError", inputDM.ItemSpec, 0, 0, 0, 0, "Failed to parse");
+            }
+        }
+    }
+}
