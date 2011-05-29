312e6abac8bf1df58602a4218713b8fed147bb72
gwatts <devnull@localhost>
2011-05-29 13:07:08 -0700

    Added the aggregate operator and also FuturePlot operators for ease of use.

--- LINQToTTree/LINQToTTreeLib/FutureResultOperators.cs
+++ LINQToTTree/LINQToTTreeLib/FutureResultOperators.cs
@@ -21,13 +21,7 @@ namespace LINQToTTreeLib
         /// <returns></returns>
         public static IFutureValue<int> FutureCount<TSource>(this IQueryable<TSource> query)
         {
-            ///
-            /// Really - we can deal with only *one* type of querable!!
-            /// 
-
-            var q = query as QueriableTTree<TSource>;
-            if (q == null)
-                throw new ArgumentException("Query must be on a TTree in order to use futures!");
+            var q = CheckSource<TSource>(query);
 
             ///
             /// Build up the count expression.
@@ -38,6 +32,19 @@ namespace LINQToTTreeLib
             var countMethod = countMethodGeneric.MakeGenericMethod(new Type[] { typeof(TSource) });
             var expr = Expression.Call(null, countMethod, query.Expression);
 
+            return FutureExecuteScalarHelper<TSource, int>(q, expr);
+        }
+
+        /// <summary>
+        /// Helper method to execute a scalar as a future.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="q"></param>
+        /// <param name="expr"></param>
+        /// <returns></returns>
+        private static IFutureValue<TResult> FutureExecuteScalarHelper<TSource, TResult>(QueriableTTree<TSource> q, MethodCallExpression expr)
+        {
             ///
             /// Generate the query model
             /// 
@@ -51,8 +58,64 @@ namespace LINQToTTreeLib
             /// 
 
             var ttree = qpb.Executor as TTreeQueryExecutor;
-            return ttree.ExecuteScalarAsFuture<int>(qm);
+            return ttree.ExecuteScalarAsFuture<TResult>(qm);
         }
 
+        /// <summary>
+        /// Check and convert the source to the proper type. Throw
+        /// if it isn't "right" for us to process with a future.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        private static QueriableTTree<TSource> CheckSource<TSource>(IQueryable<TSource> query)
+        {
+            ///
+            /// Really - we can deal with only *one* type of querable!!
+            /// 
+
+            var q = query as QueriableTTree<TSource>;
+            if (q == null)
+                throw new ArgumentException("Query must be on a TTree in order to use futures!");
+            return q;
+        }
+
+        /// <summary>
+        /// Run the future aggregate function
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TAccumulate"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="seed"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static IFutureValue<TAccumulate> FutureAggregate<TSource, TAccumulate>(this IQueryable<TSource> source, TAccumulate seed, Expression<Func<TAccumulate, TSource, TAccumulate>> func)
+        {
+            ///
+            /// Make sure the source is in good shape.
+            /// 
+
+            var q = CheckSource(source);
+
+            ///
+            /// Build the aggragate expression.
+            /// 
+
+            var aggregateMethodGeneric = typeof(Queryable).GetMethods().Where(m => m.Name == "Aggregate").Where(m => m.GetParameters().Length == 3).First();
+
+            var aGenericArgs = new Type[]
+            {
+                typeof(TSource),
+                typeof(TAccumulate)
+            };
+            var aggregateMethod = aggregateMethodGeneric.MakeGenericMethod(aGenericArgs);
+            var expr = Expression.Call(null, aggregateMethod, source.Expression, Expression.Constant(seed), Expression.Quote(func));
+
+            ///
+            /// Finally, turn this into a query and run the scalar
+            /// 
+
+            return FutureExecuteScalarHelper<TSource, TAccumulate>(q, expr);
+        }
     }
 }
--- LINQToTTree/LINQToTTreeLib/Helpers.cs
+++ LINQToTTree/LINQToTTreeLib/Helpers.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
+using LinqToTTreeInterfacesLib;
 
 namespace LINQToTTreeLib
 {
@@ -90,6 +91,31 @@ namespace LINQToTTreeLib
         }
 
         /// <summary>
+        /// given an object, apply each iteration to that object using a func, and return the object when done. Can
+        /// be used much like Aggregate (o.Fill(arg)). Result is in the future.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="seed"></param>
+        /// <param name="apply"></param>
+        /// <returns></returns>
+        public static IFutureValue<TResult> FutureApplyToObject<TSource, TResult>(this IQueryable<TSource> source, TResult seed, Expression<Action<TResult, TSource>> apply)
+        {
+            var applyMethod = typeof(Helpers).GetMethod("ApplyReturnFirst").MakeGenericMethod(typeof(TResult), typeof(TSource));
+
+            var sParameter = Expression.Parameter(typeof(TResult), "s");
+            var nParameter = Expression.Parameter(typeof(TSource), "n");
+
+            var func = Expression.Lambda<Func<TResult, TSource, TResult>>(
+                Expression.Call(applyMethod, sParameter, nParameter, apply),
+                sParameter,
+                nParameter);
+
+            return source.FutureAggregate(seed, func);
+        }
+
+        /// <summary>
         /// Create a TH1F plot from a stream of objects (with a lambda function to give flexability in conversion).
         /// </summary>
         /// <typeparam name="TSource"></typeparam>
@@ -120,6 +146,40 @@ namespace LINQToTTreeLib
 
             var lambda = Expression.Lambda<Action<ROOTNET.NTH1F, TSource>>(callFill, hParameter, vParameter);
             return source.ApplyToObject(new ROOTNET.NTH1F(plotID, plotTitle, nbins, lowBin, highBin), lambda);
+        }
+
+        /// <summary>
+        /// Create a TH1F plot from a stream of objects (with a lambda function to give flexability in conversion).
+        /// For future evaluation
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="plotID"></param>
+        /// <param name="plotTitle"></param>
+        /// <param name="nbins"></param>
+        /// <param name="lowBin"></param>
+        /// <param name="highBin"></param>
+        /// <param name="getter"></param>
+        /// <returns></returns>
+        public static IFutureValue<ROOTNET.Interface.NTH1F> FuturePlot<TSource>
+            (
+            this IQueryable<TSource> source,
+            string plotID, string plotTitle,
+            int nbins, double lowBin, double highBin,
+            Expression<Func<TSource, double>> getter)
+        {
+            var hParameter = Expression.Parameter(typeof(ROOTNET.NTH1F), "h");
+            var vParameter = Expression.Parameter(typeof(TSource), "v");
+
+            /// h.Fill(getter(v)) is what we want to code up
+
+            var callGetter = Expression.Invoke(getter, vParameter);
+
+            var fillMethod = typeof(ROOTNET.NTH1F).GetMethod("Fill", new Type[] { typeof(double) });
+            var callFill = Expression.Call(hParameter, fillMethod, callGetter);
+
+            var lambda = Expression.Lambda<Action<ROOTNET.Interface.NTH1F, TSource>>(callFill, hParameter, vParameter);
+            return source.FutureApplyToObject(new ROOTNET.NTH1F(plotID, plotTitle, nbins, lowBin, highBin), lambda);
         }
 
         /// <summary>
@@ -161,6 +221,45 @@ namespace LINQToTTreeLib
         }
 
         /// <summary>
+        /// Fill a 2D plot in the future
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="plotID"></param>
+        /// <param name="plotTitle"></param>
+        /// <param name="xNBins"></param>
+        /// <param name="xLowBin"></param>
+        /// <param name="xHighBin"></param>
+        /// <param name="yNBins"></param>
+        /// <param name="yLowBin"></param>
+        /// <param name="yHighBin"></param>
+        /// <param name="getter"></param>
+        /// <returns></returns>
+        public static IFutureValue<ROOTNET.Interface.NTH2F> FuturePlot<TSource>
+            (
+            this IQueryable<TSource> source,
+            string plotID, string plotTitle,
+            int xNBins, double xLowBin, double xHighBin,
+            int yNBins, double yLowBin, double yHighBin,
+            Expression<Func<TSource, double>> xGetter,
+            Expression<Func<TSource, double>> yGetter
+            )
+        {
+            var hParameter = Expression.Parameter(typeof(ROOTNET.Interface.NTH2F), "h");
+            var vParameter = Expression.Parameter(typeof(TSource), "v");
+
+            var callXGetter = Expression.Invoke(xGetter, vParameter);
+            var callYGetter = Expression.Invoke(yGetter, vParameter);
+
+            var fillMethod = typeof(ROOTNET.NTH2F).GetMethod("Fill", new Type[] { typeof(double), typeof(double) });
+            var callFill = Expression.Call(hParameter, fillMethod, callXGetter, callYGetter);
+
+            var lambda = Expression.Lambda<Action<ROOTNET.Interface.NTH2F, TSource>>(callFill, hParameter, vParameter);
+            ROOTNET.Interface.NTH2F interfaceobj = new ROOTNET.NTH2F(plotID, plotTitle, xNBins, xLowBin, xHighBin, yNBins, yLowBin, yHighBin);
+            return source.FutureApplyToObject(interfaceobj, lambda);
+        }
+
+        /// <summary>
         /// Generate a TH1F from a stream of T's that can be converted to a double.
         /// </summary>
         /// <typeparam name="TSource"></typeparam>
@@ -180,6 +279,28 @@ namespace LINQToTTreeLib
             where T : IConvertible
         {
             return source.Plot(plotID, plotTitle, nbins, lowBin, highBin, v => Convert.ToDouble(v));
+        }
+
+        /// <summary>
+        /// Generate a TH1F (in the future) from a stream of T's that can be converted to a double.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="plotID"></param>
+        /// <param name="plotTitle"></param>
+        /// <param name="nbins"></param>
+        /// <param name="lowBin"></param>
+        /// <param name="highBin"></param>
+        /// <returns></returns>
+        public static IFutureValue<ROOTNET.Interface.NTH1F> FuturePlot<T>
+            (
+            this IQueryable<T> source,
+            string plotID, string plotTitle,
+            int nbins, double lowBin, double highBin
+            )
+            where T : IConvertible
+        {
+            return source.FuturePlot(plotID, plotTitle, nbins, lowBin, highBin, v => Convert.ToDouble(v));
         }
 
         /// <summary>
