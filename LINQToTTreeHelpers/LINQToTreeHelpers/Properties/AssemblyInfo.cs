cf3369cb0c7c669403a3e1426b9534f023cbabe7
gwatts <devnull@localhost>
2011-06-06 00:23:01 -0700

    Make sure the build generates xml docs, and also fix up the version number.

--- LINQToTTreeHelpers/LINQToTreeHelpers/Properties/AssemblyInfo.cs
+++ LINQToTTreeHelpers/LINQToTreeHelpers/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LINQToTreeHelpers")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helper functions for dealing with the LINTToTTree library (lots of stuff on futures, some ROOT stuff, etc.)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("G. Watts/UW Seattle")]
 [assembly: AssemblyProduct("LINQToTreeHelpers")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
