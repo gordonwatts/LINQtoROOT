5dc2b7bb6072cd1acb8d3377d8a5f597659c0584
gwatts <devnull@localhost>
2011-06-15 16:38:28 -0400

    Opes - need to return the initial value as well!

--- LINQToTTree/LinqToTTreeInterfacesLib/IQVScalarResultOperator.cs
+++ LINQToTTree/LinqToTTreeInterfacesLib/IQVScalarResultOperator.cs
@@ -18,6 +18,18 @@ namespace LinqToTTreeInterfacesLib
         /// <returns></returns>
         bool CanHandle(Type resultOperatorType);
 
-        Expression ProcessResultOperator(ResultOperatorBase resultOperator, QueryModel queryModel, IGeneratedQueryCode _codeEnv, ICodeContext _codeContext, CompositionContainer container);
+        /// <summary>
+        /// Process the result operator, returning an expression that is the result, and also
+        /// an initial value that the accumulator should be set to if needed. It has to be done out
+        /// one from here b/c we may end up having to declare this in the class rather than in the code
+        /// if it is at the outter level!
+        /// </summary>
+        /// <param name="resultOperator"></param>
+        /// <param name="queryModel"></param>
+        /// <param name="_codeEnv"></param>
+        /// <param name="_codeContext"></param>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        Tuple<Expression, IValue> ProcessResultOperator(ResultOperatorBase resultOperator, QueryModel queryModel, IGeneratedQueryCode _codeEnv, ICodeContext _codeContext, CompositionContainer container);
     }
 }
