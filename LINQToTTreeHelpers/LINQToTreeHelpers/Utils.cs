cd746cb5872fa2808570b7dcc39870ea5ba355c4
gwatts <devnull@localhost>
2011-07-17 23:11:19 +0200

    Added a remove spaces method to santize strings.

--- LINQToTTreeHelpers/LINQToTreeHelpers/Utils.cs
+++ LINQToTTreeHelpers/LINQToTreeHelpers/Utils.cs
@@ -3,6 +3,12 @@ namespace LINQToTreeHelpers
 {
     internal static class Utils
     {
+        /// <summary>
+        /// Write out an object. Eventually, with ROOTNET improvements this will work better and perahps
+        /// won't be needed!
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="dir"></param>
         public static void InternalWriteObject(this ROOTNET.Interface.NTObject obj, ROOTNET.Interface.NTDirectory dir)
         {
             var h = obj as ROOTNET.Interface.NTH1;
@@ -17,6 +23,25 @@ namespace LINQToTreeHelpers
                 dir.WriteTObject(obj);
                 obj.SetNull();
             }
+        }
+
+        /// <summary>
+        /// Take a string and "sanatize" it for a root name.
+        /// </summary>
+        /// <param name="name">Text name to be used as a ROOT name</param>
+        /// <returns>argument name with spaces removes, as well as other characters</returns>
+        public static string FixupForROOTName(this string name)
+        {
+            var result = name.Replace(" ", "");
+            result = result.Replace("_{", "");
+            result = result.Replace("{", "");
+            result = result.Replace("}", "");
+            result = result.Replace("-", "");
+            result = result.Replace("\\", "");
+            result = result.Replace("%", "");
+            result = result.Replace("<", "");
+            result = result.Replace(">", "");
+            return result;
         }
     }
 }
