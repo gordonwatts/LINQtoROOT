ae51eed24f46e49ab47fae74b1d5c6065e358227
gwatts <devnull@localhost>
2011-07-30 22:13:00 -0700

    Code that runs the sub-executor and does the deed.

---
+++ LINQToTTree/QueryExecutor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using LINQToTTreeLib.ExecutionCommon.ParallelExes;
+
+namespace QueryExecutor
+{
+    class Program
+    {
+        /// <summary>
+        /// Simple program to execute a query
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+                throw new ArgumentException("Incorrect number of args");
+
+            var input = new FileInfo(args[0]);
+            if (!input.Exists)
+                throw new FileNotFoundException("Unable to find file", input.FullName);
+
+            var executor = new SubProcessRunner();
+            executor.Run(input);
+        }
+    }
+}
