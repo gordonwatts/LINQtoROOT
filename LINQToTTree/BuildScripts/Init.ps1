a148b7a825a25ed5a833f65349794a102cfa9046
gwatts <devnull@localhost>
2011-08-31 05:06:46 -0500

    Minor improv on docs

--- LINQToTTree/BuildScripts/Init.ps1
+++ LINQToTTree/BuildScripts/Init.ps1
@@ -6,6 +6,6 @@ param($installPath, $toolsPath, $package, $project)
 Import-Module -Force "$toolsPath\LINQToTTreeCommands.psm1"
 
 Write-Host "Availible commands for LINQToTTree package:"
-Write-Host "Parse-TTree <path-to-root-file>"
+Write-Host "Write-TTree-MetaData <path-to-root-file>"
 Write-Host "  Parses all TTree's in root file and generates meta data needed"
 Write-Host "  to run the TTree. Inserts resulting files in the current default project."
