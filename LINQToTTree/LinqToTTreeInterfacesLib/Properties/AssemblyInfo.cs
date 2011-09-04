437d6313b2b147637d4723c85bf1bf4a3df14137
gwatts <devnull@localhost>
2011-09-04 02:15:45 -0700

    Updates the version numbers.

--- LINQToTTree/LinqToTTreeInterfacesLib/Properties/AssemblyInfo.cs
+++ LINQToTTree/LinqToTTreeInterfacesLib/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.42.*")]
-[assembly: AssemblyFileVersion("0.42.*")]
+[assembly: AssemblyFileVersion("0.42.0.0")]
