7d9ed34a24bdaac41ffb012091fa0cd7c9c3c24f
gwatts <devnull@localhost>
2011-10-14 11:27:01 -0700

    Move sub-expression resolution into the main expression resolveer - we need it to happen in sequence with various parameter replacement. Also, when a method invokation runs, make sure that we are properly translating the arguments.

--- LINQToTTree/LINQToTTreeLib/Expressions/SubQueryExpressionResolver.cs
+++ LINQToTTree/LINQToTTreeLib/Expressions/SubQueryExpressionResolver.cs
@@ -29,68 +29,6 @@ namespace LINQToTTreeLib.Expressions
         /// </summary>
         private class Visitor : ExpressionTreeVisitor
         {
-            /// <summary>
-            /// Run a sub-query for the user. The sub-query is a full-blown expression that
-            /// must usually run in its own loop (or similar).
-            /// </summary>
-            /// <remarks>
-            /// The query visitor must be re-invoked - this ends up being a full-blown
-            /// parsing.
-            /// </remarks>
-            /// <param name="expression"></param>
-            /// <returns></returns>
-            protected override Expression VisitSubQueryExpression(SubQueryExpression expression)
-            {
-                if (MEFContainer == null)
-                    throw new InvalidOperationException("MEFContainer can't be null if we need to analyze a sub query!");
-
-                QueryVisitor qv = new QueryVisitor(GeneratedCode, CodeContext, MEFContainer);
-                qv.SubExpressionParse = true;
-                MEFContainer.SatisfyImportsOnce(qv);
-
-                ///
-                /// Run it - since this result is out of this loop, we pop-back-out when done.
-                /// 
-
-                var scope = GeneratedCode.CurrentScope;
-                qv.VisitQueryModel(expression.QueryModel);
-
-                ///
-                /// Two possible results from the sub-expression query, and how we proceed depends
-                /// on what happened in the sub query
-                /// 
-                /// 1. <returns a value> - an operator like Count() comes back from the sequence.
-                ///    it will get used in some later sequence (like # of jets in each event). So,
-                ///    we need to make sure it is declared and kept before it is used. The # that comes
-                ///    back needs to be used outside the scope we are sitting in - the one that we were at
-                ///    when we started this. Since this is a sub-query expression, the result isn't the final
-                ///    result, so we need to reset it so no one notices it.
-                /// 2. <return a sequence> - this is weird - What we are actually doing here is putting the
-                ///    sequence into code. So the loop variable has been updated with the new sequence iterator
-                ///    value. But there isn't really a result! So the result will be null...
-                /// 
-
-                var r = GeneratedCode.ResultValue;
-                if (r != null)
-                {
-                    GeneratedCode.CurrentScope = scope;
-                    if (r is IDeclaredParameter)
-                    {
-                        GeneratedCode.Add(r as IDeclaredParameter);
-                    }
-                    GeneratedCode.ResetResult();
-                    return r;
-                }
-
-                //
-                // The fact that we returned null means we are dealing with a
-                // sequence. There really is no translated version of this expression
-                // in that case - so we will return null. If someone above is depending
-                // on doing something with it they are going to run into some
-                // trouble!
-
-                return null;
-            }
 
             /// <summary>
             /// The MEF container building objects.
