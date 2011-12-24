16b344a36bca24b5f32274e1e99f6d4aaabf48f6
gwatts <devnull@localhost>
2011-12-23 22:29:01 -0600

    Make sure array length parsing works correctly for index arrays (arrays that use another variable to determine how long they are).

--- LINQToTTree/TTreeParserCPP.Tests/CreateTrees.h
+++ LINQToTTree/TTreeParserCPP.Tests/CreateTrees.h
@@ -167,6 +167,30 @@ namespace TTreeParserCPPTests {
 
 			return gcnew ROOTNET::NTTree(t);
 		}
+
+		/// Create a tree with some number of TLZ's.
+		static ROOTNET::NTTree ^CreateTreeWithIndexedSimpleVector(int entries)
+		{
+			int bogus[20];
+			int ientries = 0;
+
+			TTree *t = new TTree("dude", "left field");
+			auto brAddrEnt = t->Branch ("n", &ientries);
+			auto brAddrArr = t->Branch ("arr[n]/I", &ientries);
+
+			for (int i = 0; i < entries; i++) {
+				ientries = 10;
+				for (int j = 0; j < ientries; j++) {
+					bogus[i] = j;
+				}
+				t->Fill();
+			}
+			t->ResetBranchAddress(brAddrEnt);
+			t->ResetBranchAddress(brAddrArr);
+
+			return gcnew ROOTNET::NTTree(t);
+		}
+
 		static ROOTNET::NTTree ^CreateTreeWithSimpleSingleVector(int entries, int vectorsize)
 		{
 			vector<int> bogus;
