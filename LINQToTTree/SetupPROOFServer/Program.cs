07b88f4e4f117c01fb5af26b5a1c440029609a25
gwatts <devnull@localhost>
2012-01-03 23:13:24 -0800

    Code to add the dataset to the PROOF server.

--- LINQToTTree/SetupPROOFServer/Program.cs
+++ LINQToTTree/SetupPROOFServer/Program.cs
@@ -10,6 +10,13 @@ namespace SetupPROOFServer
         const string testDatasetName = "LINQTest";
 
         /// <summary>
+        /// List of file paths that can be found on the PROOF server.
+        /// </summary>
+        static string[] testDataSetFileList = {
+                                                 "/phys/groups/tev/scratch3/users/btag/mc11/group.perf-flavtag.105016.J7_pythia_jetjet.BTAG_D3PD.PCTF.r2725.0.111203145120/group.perf-flavtag.50938_004742.EXT0._00003.BTAG_D3PD.pool.root",
+                                       };
+
+        /// <summary>
         /// Simple setup for a PROOF server - so we can get it configured so all of our tests
         /// will run correctly.
         /// </summary>
@@ -23,6 +30,7 @@ namespace SetupPROOFServer
             if (args.Length == 0)
             {
                 Console.WriteLine("Usage: SetupPROOFServer <proof-server>");
+                return;
             }
 
             string proofURI = string.Format("proof://{0}", args[0]);
@@ -40,9 +48,32 @@ namespace SetupPROOFServer
             if (connection.ExistsDataSet(testDatasetName))
             {
                 Console.WriteLine("The test dataset exists");
+                return;
             }
 
             Console.WriteLine("Creating dataset {0}.", testDatasetName);
+
+            //
+            // Create & define the dataset.
+            //
+
+            var files = new ROOTNET.NTFileCollection(testDatasetName, string.Format("File list for {0}", testDatasetName));
+            foreach (var f in testDataSetFileList)
+            {
+                var finfo = new ROOTNET.NTFileInfo(f);
+                files.Add(finfo);
+            }
+            Console.WriteLine("Dataset {0} will contain {1} files.", testDatasetName, testDataSetFileList.Length);
+
+            if (!connection.RegisterDataSet(testDatasetName, files))
+            {
+                Console.WriteLine("Registration of the dataset unsuccessful");
+                return;
+            }
+
+            Console.WriteLine("Dataset registered");
+
+            connection.Close();
         }
     }
 }
