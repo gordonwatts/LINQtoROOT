9ca50d17affe6ed1ffbac0af2a9c1cb9baf878e3
gwatts <devnull@localhost>
2012-01-15 21:11:00 +0100

    A new statement that produces code looping over a set of paired indicies.

---
+++ LINQToTTree/LINQToTTreeLib.Tests/Statements/t_StatementRecordPairValues.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using LinqToTTreeInterfacesLib;
+using LINQToTTreeLib.Statements;
+using LINQToTTreeLib.Utils;
+using Microsoft.Pex.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LINQToTTreeLib.Tests
+{
+
+
+    /// <summary>
+    ///This is a test class for StatementRecordPairValuesTest and is intended
+    ///to contain all StatementRecordPairValuesTest Unit Tests
+    ///</summary>
+    [TestClass]
+    [PexClass(typeof(StatementRecordPairValues))]
+    public partial class StatementRecordPairValuesTest
+    {
+        [TestInitialize]
+        public void initTest()
+        {
+            TypeUtils._variableNameCounter = 0;
+        }
+
+        /// <summary>
+        ///A test for CodeItUp
+        ///</summary>
+        [PexMethod]
+        public string CodeItUpTest([PexAssumeUnderTest] StatementRecordPairValues target)
+        {
+            var actual = target.CodeItUp().ToArray();
+            Assert.AreEqual(1, actual.Length, "only xpected one line");
+            return actual[0];
+        }
+
+        /// <summary>
+        ///A test for RenameVariable
+        ///</summary>
+        [PexMethod]
+        public StatementRecordPairValues RenameVariableTest([PexAssumeUnderTest] StatementRecordPairValues target, string origin, string final)
+        {
+            target.RenameVariable(origin, final);
+            var finder = new Regex(string.Format("\b{0}\b", origin));
+            var hasit = target.CodeItUp().Where(s => finder.IsMatch(s)).Any();
+            Assert.IsFalse(hasit, "found some code that contained the original guy");
+            return target;
+        }
+
+        [PexMethod]
+        public static void TestTryCombine([PexAssumeUnderTest] StatementRecordPairValues target, IStatement statement)
+        {
+            var canComb = target.TryCombineStatement(statement, null);
+            Assert.IsNotNull(statement, "Second statement null should cause a failure");
+            var allSame = target.CodeItUp().Zip(statement.CodeItUp(), (f, s) => f == s).All(t => t);
+            Assert.AreEqual(allSame, canComb, "not expected combination!");
+        }
+
+        [TestMethod]
+        public static void TestMultipleValuesSame()
+        {
+            // Make sure we can deal with when more then one item has the same value.
+            Assert.Inconclusive("ops");
+        }
+    }
+}
---
+++ LINQToTTree/LINQToTTreeLib/Statements/StatementRecordPairValues.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using LinqToTTreeInterfacesLib;
+
+namespace LINQToTTreeLib.Statements
+{
+    /// <summary>
+    /// Record everything into a map - pair-values for later lookup.
+    /// </summary>
+    public class StatementRecordPairValues : IStatement
+    {
+        private IDeclaredParameter _mapRecord;
+        private IValue _index;
+        private IValue _indexValue;
+
+        /// <summary>
+        /// Save how we are going to go after the statement and generate it.
+        /// </summary>
+        /// <param name="mapStorage"></param>
+        /// <param name="indexVar"></param>
+        /// <param name="indexValue"></param>
+        public StatementRecordPairValues(IDeclaredParameter mapStorage, IValue indexVar, IValue indexValue)
+        {
+            //
+            // Input checks.
+            //
+
+            if (mapStorage == null)
+                throw new ArgumentNullException("mapStorage");
+            if (indexVar == null)
+                throw new ArgumentNullException("indexVar");
+            if (indexValue == null)
+                throw new ArgumentNullException("indexValue");
+
+            //
+            // Save for later
+            //
+
+            this._mapRecord = mapStorage;
+            this._index = indexVar;
+            this._indexValue = indexValue;
+        }
+
+        /// <summary>
+        /// Dump out the code for this
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> CodeItUp()
+        {
+            yield return string.Format("{0}[{1}].push_back({2});", _mapRecord.RawValue, _index.RawValue, _indexValue.RawValue);
+        }
+
+        /// <summary>
+        /// Rename a variable we are using.
+        /// </summary>
+        /// <param name="originalName"></param>
+        /// <param name="newName"></param>
+        public void RenameVariable(string originalName, string newName)
+        {
+            _index.RenameRawValue(originalName, newName);
+            _indexValue.RenameRawValue(originalName, newName);
+            _mapRecord.RenameRawValue(originalName, newName);
+        }
+
+        /// <summary>
+        /// Can we combine? Yes, if and only if the same guy and same variables!
+        /// </summary>
+        /// <param name="statement"></param>
+        /// <param name="optimize"></param>
+        /// <returns></returns>
+        public bool TryCombineStatement(IStatement statement, ICodeOptimizationService optimize)
+        {
+            if (statement.GetType() != typeof(StatementRecordPairValues))
+                return false;
+            var other = statement as StatementRecordPairValues;
+            if (other._index.RawValue != _index.RawValue)
+                return false;
+            if (other._indexValue.RawValue != _indexValue.RawValue)
+                return false;
+            if (other._mapRecord.RawValue != _mapRecord.RawValue)
+                return false;
+            return true;
+        }
+
+        /// <summary>
+        /// Get/Set the statement we are sitting in.
+        /// </summary>
+        public IStatement Parent { get; set; }
+    }
+}
