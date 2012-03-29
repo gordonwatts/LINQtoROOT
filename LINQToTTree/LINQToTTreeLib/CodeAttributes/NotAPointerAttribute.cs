6f986c985f3256a17f009f07efa9443a073c66eb
gwatts <devnull@localhost>
2012-03-28 22:26:39 -0700

    New code attribute to make the previous stuff work correctly.

---
+++ LINQToTTree/LINQToTTreeLib/CodeAttributes/NotAPointerAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LINQToTTreeLib.CodeAttributes
+{
+    /// <summary>
+    /// Applied to a field indicates during translation that it shouldn't turn into a
+    /// "*" reference, but rather a pointer.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public sealed class NotAPointerAttribute : Attribute
+    {
+        public NotAPointerAttribute()
+        {
+        }
+
+    }
+}
