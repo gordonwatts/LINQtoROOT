9609301450ed8855f08a862aee030b1049185175
gwatts <devnull@localhost>
2012-03-29 00:03:55 -0700

    Fix up test to run with the new attributes.

--- LINQToTTree/LINQToTTreeLib.Tests/Expressions/t_ExpressionsOnImpliedObjects.cs
+++ LINQToTTree/LINQToTTreeLib.Tests/Expressions/t_ExpressionsOnImpliedObjects.cs
@@ -35,13 +35,13 @@ namespace LINQToTTreeLib.Tests.Expressions
             MEFUtilities.MyClassDone();
         }
 
-        [TClonesArrayImpliedClass]
         public class EventInfo_p3 : IExpressionHolder
         {
             public Expression HeldExpression { get; private set; }
             public EventInfo_p3(Expression expr) { HeldExpression = expr; }
 
 #pragma warning disable 0649
+            [NotAPointer]
             public uint[] m_AllTheData;
 #pragma warning restore 0649
         }
@@ -53,18 +53,31 @@ namespace LINQToTTreeLib.Tests.Expressions
             public GenEvent_p4(Expression expr) { HeldExpression = expr; }
 
 #pragma warning disable 0649
+            [NotAPointer]
             public int[] m_signalProcessId;
+            [NotAPointer]
             public int[] m_eventNbr;
+            [NotAPointer]
             public double[] m_eventScale;
+            [NotAPointer]
             public double[] m_alphaQCD;
+            [NotAPointer]
             public double[] m_alphaQED;
+            [NotAPointer]
             public int[] m_signalProcessVtx;
+            [NotAPointer]
             public double[][] m_weights;
+            [NotAPointer]
             public double[][] m_pdfinfo;
+            [NotAPointer]
             public int[][] m_randomStates;
+            [NotAPointer]
             public uint[] m_verticesBegin;
+            [NotAPointer]
             public uint[] m_verticesEnd;
+            [NotAPointer]
             public uint[] m_particlesBegin;
+            [NotAPointer]
             public uint[] m_particlesEnd;
 #pragma warning restore 0649
         }
@@ -76,14 +89,23 @@ namespace LINQToTTreeLib.Tests.Expressions
             public GenVertex_p4(Expression expr) { HeldExpression = expr; }
 
 #pragma warning disable 0649
+            [NotAPointer]
             public float[] m_x;
+            [NotAPointer]
             public float[] m_y;
+            [NotAPointer]
             public float[] m_z;
+            [NotAPointer]
             public float[] m_t;
+            [NotAPointer]
             public int[][] m_particlesIn;
+            [NotAPointer]
             public int[][] m_particlesOut;
+            [NotAPointer]
             public int[] m_id;
+            [NotAPointer]
             public float[][] m_weights;
+            [NotAPointer]
             public int[] m_barcode;
 #pragma warning restore 0649
         }
@@ -95,30 +117,44 @@ namespace LINQToTTreeLib.Tests.Expressions
             public GenParticle_p4(Expression expr) { HeldExpression = expr; }
 
 #pragma warning disable 0649
+            [NotAPointer]
             public float[] m_px;
+            [NotAPointer]
             public float[] m_py;
+            [NotAPointer]
             public float[] m_pz;
+            [NotAPointer]
             public float[] m_m;
+            [NotAPointer]
             public int[] m_pdgId;
+            [NotAPointer]
             public int[] m_status;
+            [NotAPointer]
             public float[] m_thetaPolarization;
+            [NotAPointer]
             public float[] m_phiPolarization;
+            [NotAPointer]
             public int[] m_prodVtx;
+            [NotAPointer]
             public int[] m_endVtx;
+            [NotAPointer]
             public int[] m_barcode;
+            [NotAPointer]
             public short[] m_recoMethod;
 #pragma warning restore 0649
         }
 
-        [TClonesArrayImpliedClass]
         public class McEventCollection_p4 : IExpressionHolder
         {
             public Expression HeldExpression { get; private set; }
             public McEventCollection_p4(Expression expr) { HeldExpression = expr; }
 
 #pragma warning disable 0649
+            [NotAPointer]
             public GenEvent_p4 m_genEvents;
+            [NotAPointer]
             public GenVertex_p4 m_genVertices;
+            [NotAPointer]
             public GenParticle_p4 m_genParticles;
 #pragma warning restore 0649
         }
@@ -129,7 +165,9 @@ namespace LINQToTTreeLib.Tests.Expressions
             public CollectionTree(Expression expr) { HeldExpression = expr; }
 
 #pragma warning disable 0649
+            [NotAPointer]
             public EventInfo_p3 EventInfo_p3_McEventInfo;
+            [NotAPointer]
             public McEventCollection_p4 McEventCollection_p4_GEN_EVENT;
 #pragma warning restore 0649
         }
