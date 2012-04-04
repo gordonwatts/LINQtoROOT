58edd1f879b4a064e48ca4c50c02b881c0a3d651
gwatts <devnull@localhost>
2012-04-04 16:28:41 -0700

    Better comments to make it more clear to myself exactly under what conditions we are using NotAPointer for.

--- LINQToTTree/LINQToTTreeLib/CodeAttributes/NotAPointerAttribute.cs
+++ LINQToTTree/LINQToTTreeLib/CodeAttributes/NotAPointerAttribute.cs
@@ -3,9 +3,18 @@
 namespace LINQToTTreeLib.CodeAttributes
 {
     /// <summary>
-    /// Applied to a field indicates during translation that it shouldn't turn into a
-    /// "*" reference, but rather a pointer.
+    /// Applied to a field in an object that is being translated. It says it is a reference,
+    /// and not a pointer. This means that when you "." off it, it should be a "." and not
+    /// a -> in C++.
     /// </summary>
+    /// <remarks>
+    /// Say you have:
+    ///   class bogus {
+    ///     [NotAPointer] int[] bogus;
+    ///   }
+    /// and you ask for bogus a, "a.bogus.Length" you will get back "a->bogus.size()", and if you
+    /// didn't have NotAPointer above, you'd get back "a->bogus->size()".
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     public sealed class NotAPointerAttribute : Attribute
     {
