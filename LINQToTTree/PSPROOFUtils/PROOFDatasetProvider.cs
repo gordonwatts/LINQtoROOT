a0e41721a717cbff3f5014525df392be3db03aa9
gwatts <devnull@localhost>
2012-04-27 02:43:21 -0700

    First attempt at forming the connection. Of course, everything promptly fails!

--- LINQToTTree/PSPROOFUtils/PROOFDatasetProvider.cs
+++ LINQToTTree/PSPROOFUtils/PROOFDatasetProvider.cs
@@ -10,10 +10,18 @@ namespace PSPROOFUtils
     [CmdletProvider("PROOFDS", ProviderCapabilities.None)]
     public class PROOFDatasetProvider : ItemCmdletProvider
     {
+        /// <summary>
+        /// Create a new powershell drive for our proof server.
+        /// </summary>
+        /// <param name="drive"></param>
+        /// <returns></returns>
         protected override PSDriveInfo NewDrive(PSDriveInfo drive)
         {
-            Console.WriteLine("In new drive");
-            return base.NewDrive(drive);
+            var b = new ProofDrive(drive);
+            b.ProofConnection = ROOTNET.NTProof.Open(drive.Root);
+            if (b.ProofConnection == null || !b.ProofConnection.IsValid())
+                throw new ArgumentException(string.Format("Unable to connect to PROOF server at '{0}'", drive.Root));
+            return b;
         }
 
         /// <summary>
@@ -24,6 +32,18 @@ namespace PSPROOFUtils
         protected override bool IsValidPath(string path)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Custom drive that is used to hold connection info.
+        /// </summary>
+        class ProofDrive : PSDriveInfo
+        {
+            public ProofDrive (PSDriveInfo basicInfo)
+                : base(basicInfo)
+            {}
+
+            public ROOTNET.Interface.NTProof ProofConnection { get; set; }
         }
     }
 }
