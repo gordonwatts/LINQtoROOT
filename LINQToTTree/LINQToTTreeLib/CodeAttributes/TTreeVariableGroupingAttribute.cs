0d6a1f7545637a990ed7329946ba346bd5c9ce3f
Gordon Watts <gordon@gordonandpaula.net>
2015-09-01 23:17:02 -0400

    Spelling fix ups

--- LINQToTTree/LINQToTTreeLib/CodeAttributes/TTreeVariableGroupingAttribute.cs
+++ LINQToTTree/LINQToTTreeLib/CodeAttributes/TTreeVariableGroupingAttribute.cs
@@ -3,8 +3,8 @@
 namespace LINQToTTreeLib.CodeAttributes
 {
     /// <summary>
-    /// Addorns a object variable reference when it is a grouping object. The variables
-    /// refered to in this object are actually arrays, and this object is grouped around them.
+    /// Adorns a object variable reference when it is a grouping object. The variables
+    /// referred to in this object are actually arrays, and this object is grouped around them.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     public sealed class TTreeVariableGroupingAttribute : Attribute
