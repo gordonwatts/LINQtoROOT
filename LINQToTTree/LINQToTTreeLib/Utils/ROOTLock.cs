d38beb73dc523433d82a220fad34215c945d2c43
Gordon Watts <gordon@gordonandpaula.net>
2018-01-13 02:38:01 -0800

    Add a ROOT lock #177

---
+++ LINQToTTree/LINQToTTreeLib/Utils/ROOTLock.cs
@@ -0,0 +1,23 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQToTTreeLib.Utils
+{
+    /// <summary>
+    /// A helper class to help lock down ROOT from multiple access
+    /// </summary>
+    public static class ROOTLock
+    {
+        private static AsyncLock _lock = new AsyncLock();
+
+        /// <summary>
+        /// Return a async lock you can await on. Make sure to do this before doing anything
+        /// with a root file.
+        /// </summary>
+        public static AsyncLock Lock => _lock;
+    }
+}
